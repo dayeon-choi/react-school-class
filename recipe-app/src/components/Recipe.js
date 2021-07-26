2e694547e1d453af19fe3787da699bab0906a1b3
dayeon-choi <dayeon2399@naver.com>
2021-07-26 10:47:41 +0900

    5일차 - 프롭스 복습 실습

---
+++ recipe-app/src/components/Recipe.js
@@ -0,0 +1,20 @@
+// recipe-app/src/components/Recipe.js
+
+const Recipe = ({ name, ingredients, steps }) => (
+  <section id={name.toLowerCase().replace(/ /g, "-")}>
+    <h1>{name}</h1>
+    <ul className="ingredients">
+      {ingredients.map((ingredient, i) => (
+        <li key={i}>{ingredient.name}</li>
+      ))}
+    </ul>
+    <section className="instructions">
+      <h2>조리 절차</h2>
+      {steps.map((step, i) => (
+        <p key={i}>{step}</p>
+      ))}
+    </section>
+  </section>
+);
+
+export default Recipe;
