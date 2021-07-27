ee46e6b413a50f53a3ccf78474763a2ad68c9427
dayeon-choi <dayeon2399@naver.com>
2021-07-27 14:11:54 +0900

    6일차 - 훅 실습

---
+++ react-ex08/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactHook from "./components/R_ReactHook";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Start React 200!</h1>
+      <p>CSS 적용하기</p>
+      <ReactHook />
+    </div>
+  );
+}
+
+export default App;
