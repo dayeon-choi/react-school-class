cd887d533fc54a655edebd15f5c9a302438a7162
dayeon-choi <dayeon2399@naver.com>
2021-07-29 15:17:09 +0900

    8일차 - 응용 실습

---
+++ react-ex11/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import ReactRef from "./R073_ReactRef";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Start React 200!</h1>
+      <ReactRef />
+    </div>
+  );
+}
+
+export default App;
