6ae608ccc6ce422ad12d3ad250408888db74fd9f
dayeon-choi <dayeon2399@naver.com>
2021-07-29 14:12:02 +0900

    8일차 - 레시피 실습

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,52 +1,123 @@
-// recipe-app/src/App.js
-import React from "react";
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
 import "./App.css";
-import Menu from "./components/Menu";
+import {
+  Navbar,
+  Container,
+  Nav,
+  NavDropdown,
+  Button,
+  Form,
+  FormControl,
+} from "react-bootstrap";
 
-const data = [
-  {
-    name: "구운 연어",
-    ingredients: [
-      { name: "연어", amount: 500, measurement: "그램" },
-      { name: "잣", amount: 1, measurement: "컵" },
-      { name: "버터 상추", amount: 2, measurement: "컵" },
-      {
-        name: "옐로 스쿼시(Yellow Squash, 호박의 한 종류)",
-        amount: 1,
-        measurement: "개",
-      },
-      { name: "올리브 오일", amount: 0.5, measurement: "컵" },
-      { name: "마늘", amount: 3, measurement: "쪽" },
-    ],
-    steps: [
-      "오븐을 350도로 예열한다.",
-      "유리 베이킹 그릇에 올리브 오일을 두른다.",
-      "연어, 마늘, 잣을 그릇에 담는다.",
-      "오븐에서 15분간 익힌다.",
-      "옐로 스쿼시를 추가하고 다시 30분간 오븐에서 익힌다.",
-      "오븐에서 그릇을 꺼내서 15분간 식힌다음에 상추를 곁들여서 내놓는다.",
-    ],
-  },
-  {
-    name: "생선 타코",
-    ingredients: [
-      { name: "흰살생선", amount: 500, measurement: "그램" },
-      { name: "치즈", amount: 1, measurement: "컵" },
-      { name: "아이스버그 상추", amount: 2, measurement: "컵" },
-      { name: "토마토", amount: 2, measurement: "개(큰것)" },
-      { name: "또띠야", amount: 3, measurement: "개" },
-    ],
-    steps: [
-      "생선을 그릴에 익힌다.",
-      "또띠야 3장 위에 생선을 얹는다.",
-      "또띠야에 얹은 생선 위에 상추, 토마토, 치즈를 얹는다.",
-    ],
-  },
-];
 function App() {
+  const data = [
+    {
+      id: 0,
+      title: "쓴 커피",
+      content: "잠을 싹 달아나게 만드는 쓰디 쓴 원두 사용",
+      price: "3200",
+      image: "https://heenice42.github.io/shop/coffee01.jpg",
+    },
+    {
+      id: 1,
+      title: "단 커피",
+      content: "쓴 맛이 하나도 없는 달달한 원두 사용",
+      price: "4300",
+      image: "https://heenice42.github.io/shop/coffee02.jpg",
+    },
+    {
+      id: 2,
+      title: "신 커피",
+      content: "코끼리 똥에서 직접 골라 구한 원두 사용",
+      price: "2200",
+      image: "https://heenice42.github.io/shop/coffee03.jpg",
+    },
+  ];
+
+  const [cdata, setCdata] = useState(data);
+
   return (
-    <div className="App">
-      <Menu recipes={data}></Menu>
+    <>
+      <div className="App">
+        <div>
+          <Navbar bg="light" expand="lg">
+            <Container>
+              <Navbar.Brand href="#home">SHOP</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                  <Nav.Link href="#home">Home</Nav.Link>
+                  <Nav.Link href="#link">Link</Nav.Link>
+                  <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">
+                      Action
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">
+                      Another action
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">
+                      Something
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">
+                      Separated link
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.4">
+                      Separated link
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+                <Form inline>
+                  <FormControl
+                    type="text"
+                    placeholder="Search"
+                    className="mr-sm-2"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </div>
+
+        <div class="p-5 mb-4 bg-light rounded-3" className="bg">
+          <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">Coffee & Tea</h1>
+            <p class="col-md-8 fs-4">Using a series of utilities</p>
+            <button class="btn btn primary btn-lg" type="button">
+              Example button
+            </button>
+          </div>
+        </div>
+
+        <div className="container">
+          <div className="row">
+            {cdata.map((a, i) => {
+              return <Card cdata={cdata[i]} />;
+            })}
+          </div>
+        </div>
+      </div>
+
+      <Route exact path="/">
+        <div>메인페이지</div>
+      </Route>
+      <Route path="/detail">
+        <div>상세페이지</div>
+      </Route>
+    </>
+  );
+}
+
+function Card({ cdata }) {
+  return (
+    <div className="col-md-4">
+      <img src={cdata.image} alt="coffee" width="30%" />
+      <h4>{cdata.title}</h4>
+      <p>{cdata.content}</p>
+      <p>{cdata.price}원</p>
     </div>
   );
 }
